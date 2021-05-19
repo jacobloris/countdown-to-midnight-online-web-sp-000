10f89b716b2df7715dfe20b778600f07af487f77
Jacob Loris <jacobloris@outlook.com>
2021-05-19 00:48:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,16 +2,16 @@
 
 def countdown(x)
   while x != 0 
-  puts "#{number} SECOND(S)"
-  number = number - 1 
+  puts "#{x} SECOND(S)"
+  x = x - 1 
 end
 return "HAPPY NEW YEAR!"
 end 
 
 def countdown_with_sleep(x)
-while number != 0
-    puts "#{number} SECOND(S)!"
-    number = number - 1
+while x != 0
+    puts "#{x} SECOND(S)!"
+    x = x - 1
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
